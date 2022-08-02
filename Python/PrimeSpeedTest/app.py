c5f32488d513aad8606023c439df296e910321f6
György Turbék <profgyuri@gmail.com>
2022-08-02 04:15:31 +0200

    Updated to algo to match the others

--- Python/PrimeSpeedTest/app.py
+++ Python/PrimeSpeedTest/app.py
@@ -1,17 +1,25 @@
 from prime_finder import PrimeFinder
 from datetime import datetime
+from multiprocessing import Process
 
+
+intervals =\
+    [100_000, 200_000, 500_000, 1_000_000,
+     2_000_000, 5_000_000, 10_000_000,
+     20_000_000, 50_000_000, 100_000_000]
 finder = PrimeFinder()
-start_time = datetime.now()
+start = datetime.now()
 
-# returns after ~2.8 seconds
-finder.calculate_first_n_primes(count=100_000)
-print(datetime.now() - start_time)
 
-# returns after ~7.5 seconds
-finder.calculate_first_n_primes(count=200_000)
-print(datetime.now() - start_time)
+def func():
+    print(start)
+    for i in intervals:
+        finder.calculate_first_n_primes(n=i)
+        print(f"{format(i, '_')} - {datetime.now() - start}")
 
-# returns after ~77 seconds
-finder.calculate_first_n_primes(count=1_000_000)
-print(datetime.now() - start_time)
+
+if __name__ == "__main__":
+    thread = Process(target=func, args=(), daemon=True)
+    thread.start()
+    thread.join(3600.0)
+    print(f"{finder.get_calculated_primes_length()} - 1 hour")
--- Python/PrimeSpeedTest/prime_finder.py
+++ Python/PrimeSpeedTest/prime_finder.py
@@ -1,47 +1,47 @@
-import math
-
-
 class PrimeFinder:
     def __init__(self):
         self.primes = [2, 3]
+        self.sqrtBase = 2
 
-    def is_prime(self, number):
-        if number == self.primes[-1]:
-            return False
+    def set_sqrt_base(self, number):
+        if number - self.sqrtBase * self.sqrtBase > self.sqrtBase + self.sqrtBase:
+            self.sqrtBase += 1
 
-        for i in range(len(self.primes)):
-            prime = self.primes[i]
+    def check_if_prime(self, number):
+        self.set_sqrt_base(number)
 
-            if number % prime == 0:
-                return False
+        for i in range(2, len(self.primes) - 1):
+            if number % self.primes[i] == 0:
+                return
 
-            if prime > math.sqrt(number):
+            if self.primes[i] > self.sqrtBase:
                 break
 
-        return True
+        self.primes.append(number)
 
-    def calculate_first_n_primes(self, count):
-        counter = len(self.primes)
-        current = self.primes[-1] + 1
+    def get_current(self):
+        current = self.primes[-1]
+        current += 6 - current % 6
 
-        while current % 6 != 0:
-            current += 2
-
-        while counter < count:
-            lower = current - 1
-            higher = current + 1
-
-            if self.is_prime(lower):
-                self.primes.append(lower)
-                counter += 1
-
-                if counter == count:
-                    break
-
-            if self.is_prime(higher):
-                self.primes.append(higher)
-                counter += 1
-
+        if current - 1 == self.primes[-1]:
+            self.check_if_prime(current + 1)
             current += 6
 
-        return self.primes
+        return current
+
+    def calculate_first_n_primes(self, n):
+        if len(self.primes) >= n:
+            return
+
+        current = self.get_current()
+
+        while len(self.primes) < n:
+            self.check_if_prime(current - 1)
+            self.check_if_prime(current + 1)
+            current += 6
+
+        if len(self.primes) > n:
+            self.primes.pop()
+
+    def get_calculated_primes_length(self):
+        return len(self.primes)
